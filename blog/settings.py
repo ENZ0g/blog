5d3a85b08bd545f75c241412a157aefd86932b74
ENZ0g <leontev_al@mail.ru>
2019-06-05 22:20:06 +0300

    staticfiles upd

--- blog/settings.py
+++ blog/settings.py
@@ -121,10 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../blog/static'))
-
 
 import django_heroku
 django_heroku.settings(locals())
